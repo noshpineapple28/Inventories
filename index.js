624e203944cbadb6eb141d6462d66279aec0bdee
nmanoucheri001 <nmanoucheri001@idtech.com>
2022-07-27 16:12:43 -0700

    edits

---
+++ index.js
@@ -0,0 +1,123 @@
+'use strict';
+
+const express = require('express');
+const socketIO = require('socket.io');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const io = socketIO(server);
+
+/****USER DATA STORAGE****/
+let users = {};
+let dm;
+
+/****SERVER REQUEST RESOLVES****/
+io.on("connection", (socket) => {
+  //server alert message
+  console.log("****USER CONNECTED****");
+
+  //add user to array of users when recieved by server
+  socket.on("add user", (data) => {
+    if (data.userType === "DM") {
+      dm = data;
+      users["DM"] = data;
+      socket.username = data.username; //adds username to socket
+      console.log(`USER ADDED  :  ${socket.username}\n`);
+    } else {
+      if (users[data.username]) {
+        io.to(data.userID).emit("retry username");
+      } else {
+        users[data.username] = data;
+        socket.username = data.username; //adds username to socket
+        console.log(`USER ADDED  :  ${socket.username}\n`);
+      }
+    }
+  });
+
+  socket.on("add npc", (npc) => {
+    users[npc.username] = npc;
+  });
+
+  socket.on("delete npc", (npc) => {
+    delete users[npc];
+    socket.broadcast.emit("delete npc", npc);
+  });
+
+  //give inventory to DM
+  socket.on("give inventory", (data) => {
+    io.to(dm.userID).emit("update inventories", [data, socket.username]);
+  });
+
+  //remove an item from an inventory
+  socket.on("remove item", (data) => {
+    io.to(users[data[0]].userID).emit("remove item", data[1]);
+  });
+
+  //add an item
+  socket.on("add item", (data) => {
+    io.to(users[data[0]].userID).emit("add item", data[1]);
+  });
+
+  //handle share request, and send to the DM
+  socket.on("share request", (data) => {
+    io.to(dm.userID).emit("share request", data);
+  });
+
+  //allow share request
+  socket.on("allow share request", (data) => {
+    io.to(users[data[0]].userID).emit("delete shared item", data[2]);
+    io.to(users[data[1]].userID).emit("add shared item", data[2]);
+  });
+
+  //handles denied request
+  socket.on("deny share request", (user) => {
+    io.to(users[user].userID).emit("deny share request");
+  });
+
+  //adds any notes made by DM to a player's notes
+  socket.on("add note", (data) => {
+    io.to(users[data[0].currentOwner].userID).emit("add note", [
+      data[0],
+      data[1],
+    ]);
+  });
+
+  //takes chat messages
+  socket.on("chat message", (data) => {
+    socket.broadcast.emit("chat message", data);
+  });
+  //takes private messages
+  socket.on("private message", (data) => {
+    //fix bug with usernames
+    if (data.note.recipient in users) {
+      //data.note.username = users[data.note.sender].username;
+      io.to(users[data.note.recipient].userID).emit("private message", data);
+      if (
+        data.note.recipient != "DM" &&
+        users[data.note.recipient].userID != dm.userID
+      )
+        io.to(users["DM"].userID).emit("private message", data);
+    } else {
+      io.to(users["DM"].userID).emit("private message", data);
+    }
+  });
+
+  //remove user once disconnected
+  socket.on("disconnect", () => {
+    delete users[socket.username];
+    console.log(`${socket.username} disconnected\n****USER DISCONNECTED****\n`);
+  });
+});
+
+//every few seconds update the online player base and update inventories
+setInterval(() => {
+  io.sockets.emit("update player list", users);
+  for (let player in users) {
+    io.to(users[player].userID).emit("take inventory");
+  }
+}, 2000);
