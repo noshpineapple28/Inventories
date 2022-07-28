4dac677476c0eea0a1430e43451dbb1fe60b8068
nmanoucheri001 <nmanoucheri001@idtech.com>
2022-07-27 19:10:33 -0700

    idk

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-server.listen(3000, () => {
+server.listen(process.env.PORT || 3000, () => {
   console.log('listening on *:3000');
 });
 
